29df39bbfe39dae3ecc7623ea47e58d19af9c57e
Jakub Kozłowski <kubukoz@gmail.com>
2024-04-17 02:15:44 +0200

    nl_types customizations are gone!

--- posixlib/src/main/resources/scala-native/nl_types.c
+++ posixlib/src/main/resources/scala-native/nl_types.c
@@ -7,10 +7,4 @@
 int scalanative_nl_setd() { return NL_SETD; };
 int scalanative_nl_cat_locale() { return NL_CAT_LOCALE; };
 #endif // Unix or Mac OS
-
-#if defined(TARGET_PLAYDATE) // bogus values to keep linker happy
-#define NL_SETD -1
-#define NL_CAT_LOCALE -1
-#endif // _WIN32
-
 #endif
