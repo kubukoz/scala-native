7ea5604208da1c4d143640b04638e78f68210307
Jakub Kozłowski <kubukoz@gmail.com>
2024-04-17 02:20:23 +0200

    wordexp changes are gone

--- posixlib/src/main/resources/scala-native/wordexp.c
+++ posixlib/src/main/resources/scala-native/wordexp.c
@@ -44,21 +44,6 @@ _Static_assert(offsetof(struct scalanative_wordexp_t, we_offs) ==
 #endif // !OpenBSD
 #endif // Unix or Mac OS
 
-#if defined(_WIN32) || defined(__OpenBSD__) ||                                 \
-    defined(TARGET_PLAYDATE) // bogus values to keep linker happy
-#define WRDE_APPEND -1
-#define WRDE_DOOFFS -1
-#define WRDE_NOCMD -1
-#define WRDE_REUSE -1
-#define WRDE_SHOWERR -1
-#define WRDE_UNDEF -1
-#define WRDE_BADCHAR -1
-#define WRDE_BADVAL -1
-#define WRDE_CMDSUB -1
-#define WRDE_NOSPACE -1
-#define WRDE_SYNTAX -1
-#endif // _WIN32
-
 // flags
 #if !defined(__OpenBSD__)
 int scalanative_wrde_append() { return WRDE_APPEND; };
@@ -78,7 +63,7 @@ int scalanative_wrde_badchar() { return WRDE_BADCHAR; };
 
 int scalanative_wrde_badval() { return WRDE_BADVAL; };
 
-`int scalanative_wrde_cmdsub() { return WRDE_CMDSUB; };
+int scalanative_wrde_cmdsub() { return WRDE_CMDSUB; };
 
 int scalanative_wrde_nospace() { return WRDE_NOSPACE; };
 
